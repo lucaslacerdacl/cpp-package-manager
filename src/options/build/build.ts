5885c1593bfda803743f3b6c12ce2c898e5d82cd
Lucas Lacerda <lucas.lacerdabh@gmail.com>
2018-11-05 13:51:19 -0200

    Fix build wildcards

--- src/options/build/build.ts
+++ src/options/build/build.ts
@@ -18,7 +18,7 @@ export default class Build {
   }
 
   private generateBinaries() {
-    const buildCommand = 'rm -rf dist && mkdir dist && g++ -c src/**/*.cpp && mv *.o dist/';
+    const buildCommand = 'rm -rf dist && mkdir dist && g++ -c src/**/*.cpp **/*.c && mv *.o dist/';
     if (this.checkFileExists('cpm.build.json')) {
       this.generateBinariesWithConfigBuildFile(buildCommand);
     } else {
